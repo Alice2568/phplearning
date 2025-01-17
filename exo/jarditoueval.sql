3c64a4cd419ed647cf6f136a55eefa6ee925bbd5
alice <alice.ternois@orange.fr>
2025-01-17 23:22:14 +0100

    maj

--- exo/jarditoueval.sql
+++ exo/jarditoueval.sql
@@ -1,4 +1,4 @@
--- Active: 1733929630764@@127.0.0.1@3306@plouf
+-- Active: 1736869092207@@127.0.0.1@3306
 DROP DATABASE IF EXISTS plouf;
 
 CREATE DATABASE jarditou_full;
@@ -17,6 +17,48 @@ CREATE TABLE client (
     Mail VARCHAR(50)
 );
 
+CREATE TABLE fournisseur (
+    FouID INT PRIMARY KEY AUTO_INCREMENT,
+    Nom VARCHAR(50),
+    Adresse VARCHAR(100),
+    CP CHAR(5),
+    Ville VARCHAR(50),
+    Tel VARCHAR(10),
+    Mail VARCHAR(50),
+    Type BIT(1)
+);
+
+CREATE TABLE rubrique (
+    RubID INT,
+    Libelle VARCHAR(50)
+);
+
+CREATE TABLE produit (
+    ProCode CHAR(6),
+    Libelle VARCHAR(100),
+    Description VARCHAR(250),
+    Photo VARCHAR(250),
+    Affichage BIT(1),
+    PrixAchat INT,
+    StockPhysique INT,
+    StockAlerte INT,
+    fournisseur_id INT,
+    rubrique_id INT
+)
+
+CREATE TABLE panier (
+    compQuantiteProduit INT,
+    compPrixVentePar FLOAT,
+    compPrixVentePro FLOAT,
+    produit CHAR,
+    commande INT
+)
+
 CREATE TABLE commande (
-    
+    ComID INT,
+    DateCommande DATE,
+    DateLivraison DATE,
+    EtatCommande VARCHAR(25),
+    EditionFacture BIT(1),
+    client_id INT
 )
