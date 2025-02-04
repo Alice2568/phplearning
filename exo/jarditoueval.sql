659d3dcd671fcbdc56ea61a179b0bfc403629c92
Marissal Alice <alice.ternois@orange.fr>
2025-02-04 16:41:05 +0100

    maj

--- exo/jarditoueval.sql
+++ exo/jarditoueval.sql
@@ -1,4 +1,4 @@
--- Active: 1736869092207@@127.0.0.1@3306
+-- Active: 1733929630764@@127.0.0.1@3306@plouf
 DROP DATABASE IF EXISTS plouf;
 
 CREATE DATABASE jarditou_full;
--- login_form.php
+++ login_form.php
@@ -1,10 +1,23 @@
 <?php 
-$bdd = new PDO('mysql:host=127.0.0.1:3306;charset=utf8;dbname=Espace_Membres','admin','Afpa1234')
+session_start();
+$bdd = new PDO('mysql:host=127.0.0.1:3306;charset=utf8;dbname=Espace_Membres','admin','Afpa1234');
 if(isset($_POST['login'])){
     if(!empty($_POST['email']) && !empty($_POST['mdp'])){
-        $email = htmlspecialchars($_POST[]);
+        $email = htmlspecialchars($_POST['email']);
         $mdp = sha1($_POST['mdp']);
-        $insertUser = $bdd->prepare('INSERT INTO Membres')
+        $insertUser = $bdd->prepare('INSERT INTO Membres(email, mdp)VALUES(?, ?)');
+        $insertUser->execute(array($email, $mdp));
+
+        $recupUser = $bdd->prepare('SELECT * FROM Membres WHERE email = ? AND mdp = ?');
+        $recupUser->execute(array($email, $mdp));
+        if($recupUser->rowCount() > 0){
+            $_SESSION['email'] = $email;
+            $_SESSION['mdp'] = $mdp;
+            $_SESSION['id'] =$recupUser->fetch()['M_ID'];
+        }
+
+       echo $_SESSION['id'];
+
     }else{
         echo "Veuillez completer tous les champs";
     }
